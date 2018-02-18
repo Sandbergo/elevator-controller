100db44b73044dd80587b1b33ac8e617b8c12183
Sandbergo <larslsa@stud.ntnu.no>
2018-02-18 15:33:05 +0100

    asc

--- orders.c
+++ orders.c
@@ -9,18 +9,12 @@
 //button_channel_matrix[N_FLOORS][N_BUTTONS];
 
 void setOrdersHigh() {
-	int updatedLight = 0;
 	for(int button = 0; button <= BUTTON_COMMAND; button++) {
 		for(int floorNum = 0; floorNum < N_FLOORS; floorNum++) {
 			if (elev_get_button_signal(button, floorNum)) {
 				//lamp_channel_matrix[button][floorNum] = 1;
 				//button_channel_matrix[button][floorNum] = 1;
 				elev_set_button_lamp(button, floorNum, 1);
-				if(!updatedLight) {
-					printf("%s%d%s%d\n", "Order for:", button
-					       , " - ", floorNum);
-				}
-				updatedLight = 1;
 			}
 		}
 	}

708c4667e31b189438c4d1833ebf4dfc81d1603d
Lars Sandberg <larslsa@stud.ntnu.no>
2018-02-18 16:43:47 +0100

    Create timer.c

---
+++ timer.c
@@ -0,0 +1,23 @@
+#include "timer.h"
+#include <stdio.h>
+
+static clock_t timerStarted; 
+static int timerActive = 0;
+static long double duration;
+
+void tmr_startTimer(double length)
+{
+	timerStarted = clock(); 
+	duration = length;
+	timerActive = true;
+}
+
+
+int getTimerStatus()
+{
+	long double deltaTime = (long double)(clock() - timerStarted) / (CLOCKS_PER_SEC);
+	if (deltaTime >= duration && timerActive)
+	{
+		return true;
+	}
+	return false;

b572dc4cb85e576e42e31d9a1130f6f275f799d2
Sandbergo <larslsa@stud.ntnu.no>
2018-02-18 14:20:37 +0100

    fghjk

--- orders.c
+++ orders.c
@@ -3,15 +3,15 @@
 #include "orders.h"
 
 // update matrix
-lamp_channel_matrix[N_FLOORS][N_BUTTONS];
-button_channel_matrix[N_FLOORS][N_BUTTONS];
+//lamp_channel_matrix[N_FLOORS][N_BUTTONS];
+//button_channel_matrix[N_FLOORS][N_BUTTONS];
 
 void setOrdersHigh() {
 	for(int button = BUTTON_CALL_UP; button <= BUTTON_COMMAND; button++) {
 		for(int floorNum = 0; floorNum <= N_FLOORS; floorNum++) {
 			if (elev_get_button_signal(button, floorNum)) {
-				lamp_channel_matrix[button][floorNum] = 1;
-				button_channel_matrix[button][floorNum] = 1;
+				//lamp_channel_matrix[button][floorNum] = 1;
+				//button_channel_matrix[button][floorNum] = 1;
 				elev_set_button_lamp(button, floorNum, 1)
 			}
 		}
